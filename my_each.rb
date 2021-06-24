2716e6a1e8766a01c016686a35d582fc2663c252
Hannah Malm <hannahemalm616@gmail.com>
2021-06-24 14:42:40 -0400

    commit tree 4f595af33158acc089ff1b14b66cf744ec9bf827

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array) # put argument(s) here
+  #start the counter
+  i = 0 
+  #calls on WHILE 
+  while i < array.length 
+    yield array[i]
+    i = i + 1 
+  end 
+  #return something here
+  return array
 end
